9ec7cd784a31231a0b9216ec90fe4e2b4bece6d9
Brandon Tjandra <btjandra15@gmail.com>
2024-09-07 23:01:47 -0400

    Started Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,41 @@
-import Image from "next/image";
+import Photo from "@/components/HomePage/Photo";
+import Social from "@/components/HomePage/Social";
+import { Button } from "@/components/ui/button"
+import { FiDownload } from "react-icons/fi"
 
-export default function Home() {
+const Home = () => {
   return (
-    <main>
-      Homepage
-    </main>
-  );
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Engineer</span>
+
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent">Brandon Tjandra</span>
+            </h1>
+
+            <p className="max-w-[500px] mb-9 text-white/80">I'm proficient in various programming languages and interested in Machine learning & Deep Learning</p>
+
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button variant="outline" size='lg' className="uppercase flex items-center gap-2">
+                <span>Download Resume</span> 
+                <FiDownload className="text-xl"/>
+              </Button>
+
+              <div className="mb-8 xl:mb-0 ">
+                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+              </div>
+            </div>
+          </div>
+
+          <div className="">
+            {/* <Photo/> */}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home;
