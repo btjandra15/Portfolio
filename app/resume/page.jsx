70ca7417e8a8333fe632608fff2ad4b6a5d8fe54
Brandon Tjandra <btjandra15@gmail.com>
2024-09-09 18:53:56 -0400

    Started Resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+"use client";
+
+import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from 'react-icons/si';
 
 const Resume = () => {
+  const about = {
+    title: 'About me',
+    description: "I'm currently a 5th year student at The City College of New York majoring in Computer Science planning to graduate in June 2025",
+    info: [
+      {
+        fieldName: "Name",
+        fieldValue: "Brandon Tjandra"
+      },
+      {
+        fieldName: "Phone",
+        fieldValue: "(917)-635-8719"
+      },
+      {
+        fieldName: "Email",
+        fieldValue: "btjandra15@gmail.com"
+      },
+      {
+        fieldName: "Experience",
+        fieldValue: "1 year"
+      },
+    ]
+  }
+
+  const experience = {
+    icon: ''
+  }
+
   return (
     <div>
       Resume Page
