7e2288865414ff455505fd0a6dcbb7b92bb889b0
Brandon Tjandra <brandon.tjandra@nyct.com>
2024-09-13 12:50:48 -0400

    Finished Portfolio

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,10 +1,91 @@
-import React from 'react'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Select, SelectContent, SelectGroup, SeleectItem, SelectLabel, SelectTrigger, SelectValue, SelectItem } from "@/components/ui/select";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Contact = () => {
+  const info = [
+    {
+      icon: <FaPhoneAlt/>,
+      title: "Phone",
+      description: "(+1) 917 635 8719",
+    },
+    {
+      icon: <FaEnvelope/>,
+      title: "Email",
+      description: "btjandra15@gmail.com",
+    },
+    {
+      icon: <FaMapMarkerAlt/>,
+      title: "Address",
+      description: "Queens, NY",
+    },
+  ]
+
   return (
-    <div>
-      Contact Page
-    </div>
+    <motion.section initial={{opacity: 0}} animate={{opacity: 1}} className="py-6">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          <div className="xl:w-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="flex-4xl text-accent">Contact me</h3>
+
+              <p className="text-white/60">
+
+              </p>
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="First Name"/>
+                <Input type="lastname" placeholder="Last Name"/>
+                <Input type="email" placeholder="Email Address"/>
+                <Input type="phone" placeholder="Phone Number"/>
+              </div>
+
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service"/>
+                </SelectTrigger>
+
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">ML Precision Analytics</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              <Textarea className="h-[200px]" placeholder="Type your message"/>
+              <Button size="md" className="max-w-40">Send Message</Button>
+            </form>
+          </div>
+
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
   )
 }
 
